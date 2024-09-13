412a80e1f6d4768c4c6f88b9644c45fd0df34f3c
khanyajara <khanyajara@gmail.com>
2024-09-13 16:56:48 +0200

    pushing to  save progress

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import css from "./checkout.css"
 import logo from "../h-removebg-preview.png"
 import line from "./Line 20.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleLeft , faBridge } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -24,6 +26,45 @@ const Checkout=()=> {
 
         <div className="line-div">
             <img src={line} className="line-0" />
+        </div>
+
+        <div className="Columns-2">
+            <div className="column-left">
+                <button className="icon-btn">
+                    <FontAwesomeIcon icon={faArrowAltCircleLeft} className="Icons" />
+                </button><h4>Confirm and pay</h4>
+                
+                <div>
+
+                   <h5> This is one of our highly recommended Suites</h5>
+                   <FontAwesomeIcon icon={faBridge} />
+                </div>
+
+
+                <div>
+                    <h5>Your Trip:</h5>
+                    <div>
+                        <h6>Date: <br/> Oct 6-19</h6>
+                        <a href="" className="edit"> Edit</a>
+                    </div>
+                    <div>
+                        <h6>Guests <br/>2 guests</h6>
+                        <a href="" className="edit"> Edit</a>
+                    </div>
+                </div>
+                  
+                  <div>
+                    <h5>Choose How To Pay</h5>
+                  </div>
+
+            </div>
+
+
+
+
+
+            <div className="column-right"></div>
+
         </div>
         </div>
        )
